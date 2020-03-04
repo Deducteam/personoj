b5679a70718e535d73dcd7216ef66d282f33ecfb
gabrielhdt <gabrielhondet@gmail.com>
2020-03-04 13:40:28 +0100

    abbreviations

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((lambdapi-mode
+  (eval set-local-abbrevs
+        '(("subtype" "⊑" nil)
+          ("upcast" "↑" nil)))
+  (eval abbrev-mode)))
