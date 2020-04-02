3826d1690c299c7b37a0cbeafb27c29b80d376fb
gabrielhdt <gabrielhondet@gmail.com>
2020-04-02 12:27:32 +0200

    new abbrev method

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,8 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((lambdapi-mode
-  (eval set-local-abbrevs
-        '(("subtype" "⊑" nil)
-          ("upcast" "↑" nil)
-          ("downcast" "↓" nil)))
-  (eval abbrev-mode)))
+  (eval define-abbrev local-abbrev-table "subtype" "⊑" nil :system t)))
