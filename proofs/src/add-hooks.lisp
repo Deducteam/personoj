e4bf4d02428c916fe1978da12900586b3be52e1f
hondet <gabrielhondet@gmail.com>
2021-11-17 12:31:04 +0100

    dropping the subgoal marker of labels

--- proofs/src/add-hooks.lisp
+++ proofs/src/add-hooks.lisp
@@ -3,9 +3,25 @@
 
 (in-package :pvs)
 
+#+allegro
+(defun prefix-no-dot (s)
+  "Take the longest prefix of S without dot."
+  (car (excl:split-re "\\." s)))
+
+#-allegro
+(require :cl-ppcre)
+#-allegro
+(defun prefix-no-dot (s)
+  "Take the longest prefix of S without dot."
+  (car (cl-ppcre:split "\\." s)))
+
+;; prefix-no-dot is used to retrieve the name of the formula declaration from a
+;; proofstate.
+
 ;; (neg-s-forms seq) to get the antecedants
 ;; (pos-s-forms seq) to get the succedents
 ;; (collect-skolem-constants) gathers all skolem constant declarations
+;; (remove-skolem-constants expr) seem to universally quantify over skolem constant declarations
 
 (defun pprint-ps-dk (ps &optional (stream *standard-output*))
   "Transform the proofstate PS into a Dedukti proposition."
@@ -44,7 +60,7 @@
   \"dk\": the proposition in Dedukti,
   \"tac\": some information on the tactic used }"
   (let ((elts
-          `((name . ,(label ps))
+          `((name . ,(prefix-no-dot (label ps)))
             (incr . ,(incr *ps-counter*))
             (path . ,(with-output-to-string (s) (pprint-path ps s)))
             (dk   . ,(with-output-to-string (s) (pprint-ps-dk ps s)))
