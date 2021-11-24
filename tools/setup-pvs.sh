278bed64e8f9df3386734def8bcc42811119be0e
hondet <gabrielhondet@gmail.com>
2021-11-24 08:40:18 +0100

    Use ocaml setup, more opam

---
+++ tools/setup-pvs.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -eu
+
+SCRIPT=$(realpath "$0")
+DIR=$(dirname "$SCRIPT")
+ROOT=$(realpath "${DIR}/..") # Root of personoj repo
+
+sudo apt-get -q install bmake perl gcc git emacs-nox
+
+## SBCL
+
+url="https://downloads.sourceforge.net/project/sbcl/sbcl/1.4.16/sbcl-1.4.16-x86-64-linux-binary.tar.bz2"
+(cd "${HOME}" || exit 1
+ curl "$url" -L | tar jx
+ ln -s sbcl* sbcl)
+
+# PVS
+(cd "$HOME" || exit 1
+ git clone https://github.com/SRI-CSL/PVS.git -q
+ cd PVS && git checkout pvs7.1)
+
+(cd "${HOME}/PVS" || exit 1
+ autoconf
+ ./configure)
+
+ EMACS="$(command -v emacs)"
+ SBCLISP_HOME="${HOME}/sbcl"
+ PVSPATH="${HOME}/PVS"
+ export PVSPATH
+
+ for p in $(find "${ROOT}"/specs/tools/prelude_patches -name '*.diff' | sort); do
+     patch "${PVSPATH}/lib/prelude.pvs" "$p"
+ done
+
+ (cd "${PVSPATH}"
+  make EMACS="$EMACS" SBCLISP_HOME="$SBCLISP_HOME" >output 2>&1)
