acb22ab2296ff8ecbc3438cec20325fa1dc64fcb
hondet <gabrielhondet@gmail.com>
2021-12-13 15:16:56 +0100

    Added test with existential quantifier

---
+++ proofs/pipeline/examples/ex/ex.ml
@@ -0,0 +1,13 @@
+let () =
+  match
+    Unix.system
+      (Filename.quote_command "psnj-pipe"
+         [ "--qfo"; "encoding/qfo.json"; "ex.log" ])
+  with
+  | Unix.WEXITED 0 -> exit 0
+  | Unix.WEXITED n ->
+      Format.eprintf "Command exited with code %d@." n;
+      exit 1
+  | Unix.WSIGNALED _ | Unix.WSTOPPED _ ->
+      Format.eprintf "Command stopped by signal";
+      exit 2
