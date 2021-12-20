af7ade7ecc11dd9df029843b6f9a668ac1b9ddff
hondet <gabrielhondet@gmail.com>
2021-12-20 09:07:22 +0100

    Documentation and simplification of pipe
    No need for files "load.lp" or "rules.lp", they are statically generated
    by the pipe.

--- proofs/psnj_toolbox/bin/pipe.ml
+++ proofs/psnj_toolbox/bin/pipe.ml
@@ -1,6 +1,39 @@
 open Feather
 open Feather.Infix
 
+(** Create file containing lambdapi commands that open the encoding. *)
+let load_enc_f =
+  let fname = Filename.temp_file "psnj_load_enc" ".lp" in
+  let oc = open_out fname in
+  let ppf = Format.formatter_of_out_channel oc in
+  Format.fprintf ppf
+    {|require open pipe.encoding.lhol 
+  pipe.encoding.pvs_cert
+  pipe.encoding.pvs_connectives
+  pipe.encoding.propositional_connectives;
+|};
+  close_out oc;
+  fname
+
+(** Create temporary files with rewrite rules used to rewrite
+    propositions. *)
+let meta_rules_f =
+  let fname = Filename.temp_file "psnj_meta_rules" ".lp" in
+  let oc = open_out fname in
+  let ppf = Format.formatter_of_out_channel oc in
+  Format.fprintf ppf
+    {|rule true ↪ top;
+rule false ↪ bot;
+rule ¬ $x ↪ not $x;
+rule $x ∧ (λ _, $y[]) ↪ conj $x $y;
+rule $x ∨ (λ _, $y[]) ↪ disj $x $y;
+rule $x ⇒ (λ _, $y[]) ↪ imp $x $y;
+rule @∀ ↪ all;
+rule @∃ ↪ ex;
+|};
+  close_out oc;
+  fname
+
 let json (content : Ezjsonm.value list) (oc : out_channel) : unit =
   let ppf = Format.formatter_of_out_channel oc in
   let depfile = Filename.temp_file "psnj_pipe" ".dep" in
@@ -16,11 +49,11 @@ let json (content : Ezjsonm.value list) (oc : out_channel) : unit =
       [
         "meta";
         "-l";
-        "load.lp";
+        load_enc_f;
         "-e";
         "require open pipe.spec;";
         "--meta-load";
-        "rules.lp";
+        meta_rules_f;
       ]
   in
   let declaration_name (obj : Ezjsonm.value) : string =
@@ -87,6 +120,39 @@ let proveit =
 
 let cmd =
   let exits = Term.default_exits in
-  let doc = "Pipeline for personoj" in
-  let man = [] in
+  let doc = "Xcheck PVS proofs in LPMT" in
+  let man =
+    [
+      `S Manpage.s_description;
+      `P
+        "$(tname) cross checks proofs of PVS specifications. It applies \
+         several transformations to the traces of proofs (left by proveit) to \
+         finally check each inference step using automatic solvers.";
+      `P
+        "$(tname) accepts as input either a PVS file, in which case a path to \
+         the binary proveit must be provided, or a log file containing the \
+         traces of the replay of proofs";
+      `P
+        "The pipe must be called in a directory where a \"lambdapi.pkg\" file \
+         wich declares \"pipe\" as root path lies. The encoding of lambda HOL, \
+         PVS-Cert and the connectives must be available as \
+         \"pipe.encoding.lhol\", \"pipe.encoding.pvs_cert\", \
+         \"pipe.encoding.propositional_connectives\", \
+         \"pipe.encoding.pvs_connectives\". All the constants and definitions \
+         that are used by the proofs and propositions and that are not part of \
+         the encoding must be in the module \"pipe.spec\".";
+      `S Manpage.s_examples;
+      `P "See the examples in pipe_tests/";
+      `S Manpage.s_bugs;
+      `P
+        "It is required to be able to parse logs because for now, only the \
+         Allegro version of PVS can produce such logs. Because Allegro CL is \
+         not free, we cannot use it on CIs and such. Accepting produced traces \
+         allows to generate statically logs with PVS Allegro and then check \
+         these logs on the CI.";
+      `P
+        "The pipe interleaves calls to external binaries (e.g. perl), to psnj \
+         itself and ocaml computations.";
+    ]
+  in
   (Term.(const process $ proveit $ src), Term.(info "pipe" ~exits ~doc ~man))
