88fc5a1847cfa7da9d5f446d39954a95170cce78
hondet <gabrielhondet@gmail.com>
2022-01-26 20:16:42 +0100

    Fix PVS url

--- tools/setup-pvs.sh
+++ tools/setup-pvs.sh
@@ -8,7 +8,7 @@ ROOT=$(realpath "${DIR}/..") # Root of personoj repo
 sudo apt install --yes emacs-nox bmake
 
 (cd "$HOME" || exit 1
- curl http://pvs.csl.sri.com/downloads/pvs7.1.0-ix86_64-Linux-sbclisp.tgz | tar xz
+ curl https://pvs.csl.sri.com/downloads/pvs7.1.0-ix86_64-Linux-sbclisp.tgz | tar -xz
  cd pvs-7.1.0 || exit 1
  sh install-sh)
 
