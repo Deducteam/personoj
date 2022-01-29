384786d4f6debd5664c254425cf9d70f9362d422
hondet <gabrielhondet@gmail.com>
2022-01-29 02:37:12 +0100

    Do not handle errors

--- prelude/export-module.lisp
+++ prelude/export-module.lisp
@@ -4,11 +4,7 @@
       (s (uiop:parse-unix-namestring out) :direction :output
                                           :if-exists :supersede
                                           :if-does-not-exist :create)
-    (handler-bind
-        ((simple-error
-           (lambda (err)
-             (format *error-output* "~&Translation of theory ~a failed:~&~a." theory err)
-             (sb-ext:exit :code 1))))
-      (pp-dk s (get-theory theory))
-      (format *standard-output* "~&Theory ~a translated.~%" theory)))
+    ;; Let it crash and stay in debugger.
+    (pp-dk s (get-theory theory))
+    (format t "~&Theory ~a translated.~%" theory))
   (sb-ext:exit))
