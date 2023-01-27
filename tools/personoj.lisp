78645018428a9f33a238d4388e929119391904a2
Gabriel Hondet <gl_ht/diplodocus@aleeas.com>
2023-01-27 19:14:17 +0100

    More robust quickstart script
    Plus CI uses the quickstart script as well

--- tools/personoj.lisp
+++ tools/personoj.lisp
@@ -15,12 +15,23 @@
 
 ;; Select a suitable make binary
 (defvar *make*
+  ;; TODO check if the binary exists
   (case (uiop:operating-system)
     (:LINUX "bmake")
     (otherwise "make")))
 
-;; NOTE: uiop:with-current-directory does not work
-(uiop:run-program (list *make* "-C" "encoding" "install")
-                  :output *standard-output*)
+(defun install-lp-encoding ()
+  ;; NOTE: uiop:with-current-directory does not work
+  (uiop:run-program (list *make* "-C" "encoding" "install")
+                    :output *standard-output*))
 
-(sb-ext:quit)
+(multiple-value-bind (out err ret)
+    (uiop:run-program "which foo" :ignore-error-status t)
+  (declare (ignore out))
+  (declare (ignore err))
+  (if (= ret 0)
+      (install-lp-encoding)
+      (format uiop:*stderr*
+              "Cannot find executable \"lambdapi\", skipping encoding installation.~%")))
+
+(uiop:quit)
