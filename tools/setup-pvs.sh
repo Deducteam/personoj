617ed5dd257afb7e77224345f0864cfd93a56951
Gabriel Hondet <gl_ht/diplodocus@aleeas.com>
2023-01-28 07:45:20 +0100

    Simplifying CI

--- tools/setup-pvs.sh
+++ tools/setup-pvs.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 #
-# Install PVS 7.1 on a Debian-like intel x86_64. The script outputs the path to
-# the PVS installation.
+# Install PVS 7.1 on a Debian-like intel x86_64 in ~. The script outputs the
+# path to the PVS installation. PVS is then available as ~/pvs-7.1.0/pvs.
 #
-set -eu
-
-SCRIPT=$(realpath "$0")
-DIR=$(dirname "$SCRIPT")
+set -o nounset
+set -o errexit
+set -o xtrace
 
 sudo apt install --yes emacs-nox bmake
 
 (cd "$HOME" || exit 1
- curl https://pvs.csl.sri.com/downloads/pvs7.1.0-ix86_64-Linux-sbclisp.tgz | tar -xz
- cd pvs-7.1.0 || exit 1
- sh install-sh)
+curl https://pvs.csl.sri.com/downloads/pvs7.1.0-ix86_64-Linux-sbclisp.tgz | tar -xz
+cd pvs-7.1.0 || exit 1
+sh install-sh)
 
 echo "${HOME}/pvs-7.1.0"
