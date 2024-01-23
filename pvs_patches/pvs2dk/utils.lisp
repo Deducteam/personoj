101073f40ca68de6969b698c8f8cd967a7980da4
gabrielhdt <koizel@tilde.club>
2024-01-23 09:42:43 +0100

    PVS uses anaphora now

--- pvs_patches/pvs2dk/utils.lisp
+++ pvs_patches/pvs2dk/utils.lisp
@@ -1,6 +1,6 @@
 (in-package :pvs)
 
-(export '(double mkstr symb aif))
+(export '(double mkstr symb))
 (proclaim '(inline double))
 
 (defun double (lst)
@@ -12,10 +12,3 @@
 
 (defun symb (&rest args)
   (values (intern (apply #'mkstr args))))
-
-(defmacro aif (test-form then-form &optional else-form)
-  "Anaphoric `if' using IT as the result of the test form."
-  (let ((it (intern (symbol-name 'it)))) ;so that the macro can be exported
-    `(let ((,it ,test-form))
-       (declare (ignorable ,it))
-       (if ,it ,then-form ,else-form))))
