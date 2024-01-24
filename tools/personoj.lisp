7ba7f5fc584951ba7c80d0d0e2c6729e7f93918d
gabrielhdt <koizel@tilde.club>
2024-01-24 10:04:51 +0100

    More thorough install procedure
    Check if prettyprint-dedukti is available

--- tools/personoj.lisp
+++ tools/personoj.lisp
@@ -7,30 +7,38 @@
 ;; Fail on debugger
 (setf *debugger-hook*
       (lambda (c h)
-        (declare (ignore c h))
+        (declare (ignore h))
+        (format *error-output* "Something went wrong during the install procedure.~
+You may report it.")
+        (princ c *error-output*)
         (uiop:quit 1)))
 
 (unless (string-equal (car (last (pathname-directory (uiop:getcwd))))
                       "personoj")
   (error "This script should be run from the root of the personoj repository."))
 
-;; TODO check if the lines have already been appended
-
-(if (or (not (interactive-stream-p *standard-input*))
-        (yes-or-no-p "Can I edit ~~/.pvs.lisp?"))
-    (with-open-file
-        (pvs.lisp #P"~/.pvs.lisp"
-                  :direction :output
-                  :if-exists :append
-                  :if-does-not-exist :create)
-      (format pvs.lisp ";;; The following lines were added by personoj~%")
+;; If prettyprint-dedukti isn't available, it means personoj isn't loaded
+(unless (handler-case (and (find-package 'pvs)
+                           (functionp (find-symbol "prettyprint-dedukti" :pvs)))
+          (undefined-function (c)
+            (declare (ignore c))
+            nil))
+  (format *query-io* "Personoj not loaded, setting up autoload.")
+  (if (or (not (interactive-stream-p *standard-input*))
+          (yes-or-no-p "Can I edit ~~/.pvs.lisp?"))
       (with-open-file
-          (load-personoj #P"tools/load-personoj.lisp"
-                         :direction :input
-                         :if-does-not-exist :error)
-        (uiop:copy-stream-to-stream load-personoj pvs.lisp :linewise t))
-      (format pvs.lisp "~%(load-personoj \"~a\")~%" (sb-posix:getcwd))
-      (format pvs.lisp ";;; The previous lines were added by personoj~%")))
+          (pvs.lisp #P"~/.pvs.lisp"
+                    :direction :output
+                    :if-exists :append
+                    :if-does-not-exist :create)
+        (format pvs.lisp ";;; The following lines were added by personoj~%")
+        (with-open-file
+            (load-personoj #P"tools/load-personoj.lisp"
+                           :direction :input
+                           :if-does-not-exist :error)
+          (uiop:copy-stream-to-stream load-personoj pvs.lisp :linewise t))
+        (format pvs.lisp "~%(load-personoj \"~a\")~%" (sb-posix:getcwd))
+        (format pvs.lisp ";;; The previous lines were added by personoj~%"))))
 
 ;; Select a suitable make binary
 (defvar *make*
@@ -40,7 +48,7 @@
 
 (defun binary-exists-p (bin)
   (multiple-value-bind (out err ret)
-      (uiop:run-program (list "command" "-v" bin) :ignore-error-status t)
+      (uiop:run-program (list "which" bin) :ignore-error-status t)
     (declare (ignore out err))
     (= ret 0)))
 
